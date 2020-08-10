cd2e8142c8a199fd2ab358112b4a8bcedbdcb9e3
char <645992678@qq.com>
2020-08-10 11:27:48 +0800

    git测试二次更新

--- src/App.java
+++ src/App.java
@@ -5,5 +5,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         StartFrame sf = new StartFrame("普通的卡牌大冒险");
         sf.setVisible(true);
+        //git测试
     }
 }
