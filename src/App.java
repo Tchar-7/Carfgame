b9cd08926631b46455974b79dcdb89e9cad6cab0
char <645992678@qq.com>
2020-10-20 16:25:14 +0800

    idea

--- src/App.java
+++ src/App.java
@@ -5,6 +5,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         StartFrame sf = new StartFrame("普通的卡牌大冒险");
         sf.setVisible(true);
-        //git测试
     }
 }
